b72c8774a73b4cc5b2a88c464d7fd3684d9e4bdf
Md. Jubayer Hossain <mjh84349@gmail.com>
2026-03-15 01:37:05 +0600

    update the some code versions in hearder.jsx file gradient - linear ; order none - order - 0

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ export default function Header() {
       <div className="text-center sm:text-left">
         <motion.h1
           className="text-2xl font-semibold tracking-tight 
-          bg-gradient-to-r from-teal-400 to-cyan-400 bg-clip-text text-transparent"
+          bg-linear-to-r from-teal-400 to-cyan-400 bg-clip-text text-transparent"
           animate={{
             backgroundPosition: ["0% 50%", "100% 50%", "0% 50%"],
           }}
@@ -37,7 +37,7 @@ export default function Header() {
       <nav
         className="
           flex items-center gap-4 sm:gap-6 text-sm
-          order-3 sm:order-none
+          order-3 sm:order-0
         "
       >
         <a href="#about" className="text-slate-300 hover:text-teal-400 transition">
@@ -58,7 +58,7 @@ export default function Header() {
       <nav
         className="
           flex items-center gap-2 sm:gap-3 text-sm
-          order-2 sm:order-none
+          order-2 sm:order-0
         "
       >
         <motion.a
@@ -76,7 +76,7 @@ export default function Header() {
         <motion.a
           href="mailto:jubayer.dev.bd@gmail.com"
           className="flex items-center gap-1 sm:gap-2 px-2 py-1 sm:px-3 sm:py-2 
-          rounded-md bg-gradient-to-r from-teal-500 to-cyan-400 
+          rounded-md bg-linear-to-r from-teal-500 to-cyan-400 
           text-slate-900 font-medium shadow hover:scale-105 transition"
           whileHover={{ scale: 1.1 }}
         >
